fb02b25fa4aadcdd2bd2315a8886ea1c5198d184
Jason Valenzuela <jason.e.valenzuela@gmail.com>
2020-06-30 19:17:07 -0500

    initial commit

---
+++ src/test/java/com/cakefactory/app/CakeFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakefactory.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
