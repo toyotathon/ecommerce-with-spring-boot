007ee02ff938e2f864ed1d52b4fd719245b6e1ee
Jason Valenzuela <jason.e.valenzuela@gmail.com>
2020-06-30 19:44:43 -0500

    adding controller, gitignore

---
+++ src/main/java/com/cakefactory/app/IndexController.java
@@ -0,0 +1,13 @@
+package com.cakefactory.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+  @GetMapping("/")
+  String index() {
+      return "index";
+  }
+  
+}
