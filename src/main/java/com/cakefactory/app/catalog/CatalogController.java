a2e1ac4d2383db0e46e9a72161a2d8e87e3646e1
Jason Valenzuela <jason.e.valenzuela@gmail.com>
2020-07-02 13:07:56 -0400

    adding some more stuff, failing on service

--- src/main/java/com/cakefactory/app/catalog/CatalogController.java
+++ src/main/java/com/cakefactory/app/catalog/CatalogController.java
@@ -1,5 +1,27 @@
 package com.cakefactory.app.catalog;
 
-public class CatalogController {
+import java.util.Map;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/catalog")
+public class CatalogController {
+  private final CatalogService catalogService;
+
+  CatalogController(CatalogService catalogService) {
+    this.catalogService = catalogService;
+  }
+
+  @GetMapping()
+  ModelAndView index() {
+    log.info(ItemUtils.getBaseItems().toString());
+    return new ModelAndView("catalog", Map.of("items", ItemUtils.getBaseItems()));
+  }
 }
